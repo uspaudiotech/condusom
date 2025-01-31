2067f0a8e9eacb3b159de433309621dd97f78b5e
haruo <takeuchigabriel@usp.br>
2025-01-31 00:09:27 -0300

    Adjust synth and audio parameters

--- src/constants.py
+++ src/constants.py
@@ -9,11 +9,11 @@ MAX_HANDS                 = 1
 NUM_LANDMARKS             = 21
 
 # Synth settings
-MIN_FREQ = 100
+MIN_FREQ = 300
 MAX_FREQ = 2**2 * MIN_FREQ
 
 # Audio settings
 SR      = 48000
 SLEEP   = 0.1
-DEF_AMP = 0.4
+DEF_AMP = 0.2
 DEF_BLOCK_SIZE = 64
